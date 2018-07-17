35d23b75da6df924946d0314bc216cd1c69c6f99
Paras Rawat <parasmyname@gmail.com>
2018-07-17 16:56:35 +0530

    QueryPage3

--- app/src/main/java/com/example/gupta/tt/Query.java
+++ app/src/main/java/com/example/gupta/tt/Query.java
@@ -1,13 +1,28 @@
 package com.example.gupta.tt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Query extends AppCompatActivity {
+    ImageView classimage, roomimage,timetableimage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_query);
+        classimage=findViewById(R.id.classimage);
+        roomimage=findViewById(R.id.roomimage);
+        timetableimage=findViewById(R.id.timetableimage);
+
+        classimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Query.this,AddTTActivity.class));
+            }
+        });
+
     }
 }
